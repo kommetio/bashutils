5504445ef6fa2a6c39e25749eff766947a075486
radek <vaihtoopiskelija@gmail.com>
2022-11-08 11:39:54 +0100

    Script for generating subdomains

---
+++ apache2-multiple-subdomains/create-subdomains.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+template_file="subdomain-template"
+
+echo "Deleting old files"
+rm sandbox*.conf
+
+# Create 20 subdomains with numbers 001, 002, ..., 020
+for i in $(seq -f "%03g" 1 20)
+do
+
+	echo "Creating subdomain ${i}"
+
+	subdomain="sandbox${i}"
+	newfile="${subdomain}.conf"
+	port="9${i}"
+	
+	# copy the template file replacing domain and port
+	sed "s/subdomainname/$subdomain/g" $template_file > $newfile
+
+done
+
+echo "Restarting apache2"
+/etc/init.d/apache2 restart
